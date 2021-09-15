347d2a6ab9b4954344410f91225a5e1cff555ed9
emmawang21 <emma.wang@rubrik.com>
2021-09-15 14:45:00 -0700

    Make traffic generator work on python3.9

---
+++ kubernetes-manifests/create-resources.sh
@@ -0,0 +1,32 @@
+set -x
+ACME_SECRET=rubrik
+ACME_NAMESPACE=acme-fitness
+kubectl create ns ${ACME_NAMESPACE}
+kubectl -n ${ACME_NAMESPACE} create secret generic cart-redis-pass --from-literal=password=${ACME_SECRET}
+# Create PVC
+kubectl -n ${ACME_NAMESPACE} apply -f mongodata_catalog.yaml
+kubectl -n ${ACME_NAMESPACE} apply -f mongodata_users.yaml
+kubectl -n ${ACME_NAMESPACE} apply -f redisdata-cart.yaml
+kubectl -n ${ACME_NAMESPACE} apply -f cart-total-data.yaml
+kubectl -n ${ACME_NAMESPACE} apply -f order-db-postgress.yaml
+
+kubectl -n ${ACME_NAMESPACE} apply -f cart-redis-total.yaml
+kubectl -n ${ACME_NAMESPACE} apply -f cart-total.yaml
+kubectl -n ${ACME_NAMESPACE} create secret generic catalog-mongo-pass --from-literal=password=${ACME_SECRET}
+kubectl -n ${ACME_NAMESPACE} create -f catalog-db-initdb-configmap.yaml
+kubectl -n ${ACME_NAMESPACE} apply -f catalog-db-total.yaml
+kubectl -n ${ACME_NAMESPACE} apply -f catalog-total.yaml
+kubectl -n ${ACME_NAMESPACE} apply -f payment-total.yaml
+kubectl -n ${ACME_NAMESPACE} create secret generic order-postgres-pass --from-literal=password=${ACME_SECRET}
+kubectl -n ${ACME_NAMESPACE} apply -f order-db-total.yaml
+kubectl -n ${ACME_NAMESPACE} apply -f order-total.yaml
+kubectl -n ${ACME_NAMESPACE} create secret generic users-mongo-pass --from-literal=password=${ACME_SECRET}
+kubectl -n ${ACME_NAMESPACE} create secret generic users-redis-pass --from-literal=password=${ACME_SECRET}
+kubectl -n ${ACME_NAMESPACE} create -f users-db-initdb-configmap.yaml
+kubectl -n ${ACME_NAMESPACE} apply -f users-db-total.yaml
+kubectl -n ${ACME_NAMESPACE} apply -f users-redis-total.yaml
+kubectl -n ${ACME_NAMESPACE} apply -f users-total.yaml
+kubectl -n ${ACME_NAMESPACE} apply -f frontend-total.yaml
+kubectl -n ${ACME_NAMESPACE} get services -l service=frontend
+
+kubectl -n ${ACME_NAMESPACE} apply -f point-of-sales-total-svc.yaml
--- traffic-generator/locustfile.py
+++ traffic-generator/locustfile.py
@@ -1,11 +1,15 @@
-# This program will generate traffic for ACME Fitness Shop App. It simulates both Authenticated and Guest user scenarios. You can run this program either from Command line or from
+# This program will generate traffic for ACME Fitness Shop App.
+# It simulates both Authenticated and Guest user scenarios.
+# You can run this program either from Command line or from
 # the web based UI. Refer to the "locust" documentation for further information. 
 
-from locust import HttpLocust, TaskSet, task, TaskSequence, seq_task, Locust
+from locust import HttpUser, TaskSet, task, SequentialTaskSet,  Locust
 import random
 
+from locust.user.users import HttpUser
+
 # List of users (pre-loaded into ACME Fitness shop)
-users = ["eric", "phoebe", "dwight", "han"] 
+users = ["eric", "phoebe", "dwight", "han"]
 
 # List of products within the catalog
 products = []
@@ -13,7 +17,7 @@ products = []
 import logging
 
 # GuestUserBrowsing simulates traffic for a Guest User (Not logged in)
-class GuestUserBrowsing(TaskSequence):
+class GuestUserBrowsing(SequentialTaskSet):
 
     def on_start(self):
         self.getProducts()
@@ -39,27 +43,26 @@ class GuestUserBrowsing(TaskSequence):
         products.clear()
 
 # AuthUserBrowsing simulates traffic for Authenticated Users (Logged in)
-class AuthUserBrowsing(TaskSequence):
+class AuthUserBrowsing(SequentialTaskSet):
 
     def on_start(self):
         self.login()
     
-    @seq_task(1)
     @task(1)
     def login(self):
         user = random.choice(users)
         logging.info("Auth User - Login user " + user)
         body = self.client.post("/login/", json={"username": user, "password":"vmware1!"}).json()
-        self.locust.userid = body["token"]
+        self.userid = body["access_token"]
+        # logging.info(f"login body: {body}")
+        # logging.info("self.userid: " + self.userid)
 
-    @seq_task(2)
-    @task(1)
+    @task(2)
     def getProducts(self):
         logging.info("Auth User - Get Catalog")
         self.client.get("/products")
 
-    @seq_task(3)
-    @task(2)
+    @task(3)
     def getProduct(self):
         logging.info("Auth User - Get a product")
         products = self.listCatalogItems()
@@ -69,27 +72,27 @@ class AuthUserBrowsing(TaskSequence):
         products.clear()
 
     
-    @seq_task(4)
-    @task(2)
+    @task(4)
     def addToCart(self):
         self.listCatalogItems()
         productid = random.choice(products)
-        logging.info("Add to Cart for user " + self.locust.userid)
-        cart = self.client.post("/cart/item/add/" + self.locust.userid, json={
+        # logging.info(f"userid: {self.userid}")
+        logging.info("Add to Cart for user " + self.userid)
+        cart = self.client.post("/cart/item/add/" + self.userid, json={
                   "name": productid,
                   "price": "100",
                   "shortDescription": "Test add to cart",
                   "quantity": random.randint(1,2),
                   "itemid": productid
                 })
+        # logging.info(cart)
         products.clear()
 
     
-    @seq_task(5)
-    @task(1)
+    @task(5)
     def checkout(self):
-        userCart = self.client.get("/cart/items/" + self.locust.userid).json()
-        order = self.client.post("/order/add/"+ self.locust.userid, json={ "userid":"8888",
+        userCart = self.client.get("/cart/items/" + self.userid).json()
+        order = self.client.post("/order/add/"+ self.userid, json={ "userid":"8888",
                 "firstname":"Eric",
                 "lastname": "Cartman",
                 "address":{
@@ -112,6 +115,7 @@ class AuthUserBrowsing(TaskSequence):
                     {"id":"5678", "description":"bluepants", "quantity":"1", "price":"4"}
                 ],
                 "total":"100"})
+        logging.info("Checkout completed")
 
 
     def listCatalogItems(self):
@@ -125,14 +129,9 @@ class AuthUserBrowsing(TaskSequence):
         self.client.get("/")
 
 
-class UserBehavior(TaskSet):
+class WebSiteUser(HttpUser):
 
     tasks = {AuthUserBrowsing:2, GuestUserBrowsing:1}
-
-
-class WebSiteUser(HttpLocust):
-
-    task_set = UserBehavior
     userid = ""
     min_wait = 2000
     max_wait = 10000
